0f700f37557bc356f7b8f6bbca403fda648f2f97
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-04-11 12:36:27 +0200

    completed main part

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="card">
+        <img :src="image" class="card-img-top" :alt="title">
+        <div class="card-body">
+            <h5 class="card-title">{{title}}</h5>
+            <p class="card-text">{{ text }}</p>
+            <a href="#" class="btn btn-primary" v-if="link">{{ link }}</a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CardComponent',
+    props: ['image', 'title', 'text','link']
+}
+</script>
+
+<style lang="scss" scoped></style>
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,14 +1,32 @@
 <template>
     <main>
         <div class="container">
-        <h2>--> Content goes here <--</h2>
+            <h2>Lista Prodotti</h2>
+            <div class="row">
+                <div class="col-12 col-md-6 col-lg-3 mb-4" v-for="(item, index) in products" :key="index">
+                    <CardComponent :image="item.src" :title="item.titolo" :text="`Tipo: ${item.tipo} - Cottura: ${item.cottura} min`" />
+                </div>
+            </div>
         </div>
     </main>
 </template>
 
 <script>
+    import { prodotti } from '../data/store.js';
+    import CardComponent from '../components/CardComponent.vue';
     export default {
-        name: 'MainComponent'
+        name: 'MainComponent',
+        components: {
+            CardComponent
+        },
+        data() {
+            return {
+                products: prodotti,
+            }
+        },
+        mounted() {
+            console.log(this.products)
+        },
     }
 </script>
 
